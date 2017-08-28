e5d80c3caa00c9b0c5db62065aaf8ebfb0fce356
tom <tom@daoshenggroup.com>
2017-08-28 14:37:29 +0800

    auto import

--- trace-rest-provider/src/main/java/org/food/safety/trace/extension/TimerFilter.java
+++ trace-rest-provider/src/main/java/org/food/safety/trace/extension/TimerFilter.java
@@ -1,25 +1,8 @@
-/**
- * Copyright 1999-2014 dangdang.com.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.food.safety.trace.extension;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -29,11 +12,9 @@ import java.time.Instant;
 
 /**
  * URL地址检查
- * 放过sigin-in,sign-ou;如果cookie不等null放过
  *
  * @author tom
  */
-@Priority(Priorities.USER)
 public class TimerFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TimerFilter.class);
